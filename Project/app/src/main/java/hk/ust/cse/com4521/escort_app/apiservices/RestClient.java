5c815f814b1dce556fcc384b1b491a6b9247c048
Wafa BEN_HADJ_SGHAIER <wafa.ben-hadj-sghaier@ensimag.grenoble-inp.fr>
2016-04-25 14:06:20 +0800

    add package apiservices

---
+++ Project/app/src/main/java/hk/ust/cse/com4521/escort_app/apiservices/RestClient.java
@@ -0,0 +1,7 @@
+package hk.ust.cse.com4521.escort_app.apiservices;
+
+/**
+ * Created by SGHAIER on 25/04/16.
+ */
+public class RestClient {
+}
---
+++ Project/app/src/main/java/hk/ust/cse/com4521/escort_app/apiservices/service/APIService.java
@@ -0,0 +1,7 @@
+package hk.ust.cse.com4521.escort_app.apiservices.service;
+
+/**
+ * Created by SGHAIER on 25/04/16.
+ */
+public interface APIService {
+}
