2264c90d15b01a4add015075b8d639ea05fcba96
Wafa BEN_HADJ_SGHAIER <wafa.ben-hadj-sghaier@ensimag.grenoble-inp.fr>
2016-04-25 13:53:33 +0800

    delete unuseful java classes

--- Project/app/src/main/java/hk/ust/cse/com4521/escort_app/API/gitapi.java
+++
@@ -1,7 +0,0 @@
-package hk.ust.cse.com4521.escort_app.API;
-
-/**
- * Created by SGHAIER on 25/04/16.
- */
-public interface gitapi {
-}
--- Project/app/src/main/java/hk/ust/cse/com4521/escort_app/MODEL/gitmodel.java
+++
@@ -1,7 +0,0 @@
-package hk.ust.cse.com4521.escort_app.MODEL;
-
-/**
- * Created by SGHAIER on 25/04/16.
- */
-public class gitmodel {
-}
