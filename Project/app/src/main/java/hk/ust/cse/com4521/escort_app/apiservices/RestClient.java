4d4d296545598b17b987c0e4f5550b0208485afd
Wafa BEN_HADJ_SGHAIER <wafa.ben-hadj-sghaier@ensimag.grenoble-inp.fr>
2016-04-25 15:38:36 +0800

    REST packages

--- Project/app/src/main/java/hk/ust/cse/com4521/escort_app/apiservices/RestClient.java
+++ Project/app/src/main/java/hk/ust/cse/com4521/escort_app/apiservices/RestClient.java
@@ -1,7 +1,32 @@
 package hk.ust.cse.com4521.escort_app.apiservices;
 
+import java.util.List;
+
+import hk.ust.cse.com4521.escort_app.apiservices.service.APIService;
+
 /**
  * Created by SGHAIER on 25/04/16.
  */
 public class RestClient {
+    private static RestClient instance = null;
+
+    //private ResultReadyCallback callback;
+
+    private static final String BASE_URL = "http://....";
+    private APIService service;
+
+
+
+
+    public static RestClient getInstance() {
+        if(instance == null) {
+            instance = new RestClient();
+        }
+        return instance;
+    }
+
+    /*public interface ResultReadyCallback {
+        public void resultReady(List<User> users);
+    }*/
+
 }
