340de1d4b113305b2b0bd3a66234ac6e1d307682
Wafa BEN_HADJ_SGHAIER <wafa.ben-hadj-sghaier@ensimag.grenoble-inp.fr>
2016-05-02 22:14:28 +0800

    retrofit token

--- Project/app/src/main/java/hk/ust/cse/com4521/escort_app/apiservices/model/MeetingHistories.java
+++ Project/app/src/main/java/hk/ust/cse/com4521/escort_app/apiservices/model/MeetingHistories.java
@@ -7,198 +7,352 @@ package hk.ust.cse.com4521.escort_app.apiservices.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MeetingHistories {
 
-    @SerializedName("escortUserId")
-    @Expose
-    private String escortUserId;
-    @SerializedName("staffId")
-    @Expose
-    private String staffId;
-    @SerializedName("receiptRefNum")
-    @Expose
-    private String receiptRefNum;
-    @SerializedName("doctorAdvice")
-    @Expose
-    private String doctorAdvice;
-    @SerializedName("status")
-    @Expose
-    private Integer status;
-    @SerializedName("patientUserId")
-    @Expose
-    private String patientUserId;
-    @SerializedName("patientRequirement")
-    @Expose
-    private String patientRequirement;
-    @SerializedName("PatientMeetingPostingID")
-    @Expose
-    private String PatientMeetingPostingID;
-    @SerializedName("id")
-    @Expose
-    private String id;
 
-    /**
-     *
-     * @return
-     * The escortUserId
-     */
-    public String getEscortUserId() {
-        return escortUserId;
-    }
 
-    /**
-     *
-     * @param escortUserId
-     * The escortUserId
-     */
-    public void setEscortUserId(String escortUserId) {
-        this.escortUserId = escortUserId;
-    }
+        @SerializedName("escortUserId")
+        @Expose
+        private String escortUserId;
+        @SerializedName("staffId")
+        @Expose
+        private String staffId;
+        @SerializedName("receiptRefNum")
+        @Expose
+        private String receiptRefNum;
+        @SerializedName("doctorAdvice")
+        @Expose
+        private String doctorAdvice;
+        @SerializedName("status")
+        @Expose
+        private Integer status;
+        @SerializedName("patientUserId")
+        @Expose
+        private String patientUserId;
+        @SerializedName("PatientMeetingPostingID")
+        @Expose
+        private String PatientMeetingPostingID;
+        @SerializedName("location")
+        @Expose
+        private String location;
+        @SerializedName("specialNeeds")
+        @Expose
+        private String specialNeeds;
+        @SerializedName("wheelchair")
+        @Expose
+        private Boolean wheelchair;
+        @SerializedName("meetingDuration")
+        @Expose
+        private Integer meetingDuration;
+        @SerializedName("meetingTimeStart")
+        @Expose
+        private Integer meetingTimeStart;
+        @SerializedName("meetingDate")
+        @Expose
+        private String meetingDate;
+        @SerializedName("district")
+        @Expose
+        private Integer district;
+        @SerializedName("dialect")
+        @Expose
+        private List<Integer> dialect = new ArrayList<Integer>();
+        @SerializedName("id")
+        @Expose
+        private String id;
 
-    /**
-     *
-     * @return
-     * The staffId
-     */
-    public String getStaffId() {
-        return staffId;
-    }
+        /**
+         *
+         * @return
+         * The escortUserId
+         */
+        public String getEscortUserId() {
+            return escortUserId;
+        }
 
-    /**
-     *
-     * @param staffId
-     * The staffId
-     */
-    public void setStaffId(String staffId) {
-        this.staffId = staffId;
-    }
+        /**
+         *
+         * @param escortUserId
+         * The escortUserId
+         */
+        public void setEscortUserId(String escortUserId) {
+            this.escortUserId = escortUserId;
+        }
 
-    /**
-     *
-     * @return
-     * The receiptRefNum
-     */
-    public String getReceiptRefNum() {
-        return receiptRefNum;
-    }
+        /**
+         *
+         * @return
+         * The staffId
+         */
+        public String getStaffId() {
+            return staffId;
+        }
 
-    /**
-     *
-     * @param receiptRefNum
-     * The receiptRefNum
-     */
-    public void setReceiptRefNum(String receiptRefNum) {
-        this.receiptRefNum = receiptRefNum;
-    }
+        /**
+         *
+         * @param staffId
+         * The staffId
+         */
+        public void setStaffId(String staffId) {
+            this.staffId = staffId;
+        }
 
-    /**
-     *
-     * @return
-     * The doctorAdvice
-     */
-    public String getDoctorAdvice() {
-        return doctorAdvice;
-    }
+        /**
+         *
+         * @return
+         * The receiptRefNum
+         */
+        public String getReceiptRefNum() {
+            return receiptRefNum;
+        }
 
-    /**
-     *
-     * @param doctorAdvice
-     * The doctorAdvice
-     */
-    public void setDoctorAdvice(String doctorAdvice) {
-        this.doctorAdvice = doctorAdvice;
-    }
+        /**
+         *
+         * @param receiptRefNum
+         * The receiptRefNum
+         */
+        public void setReceiptRefNum(String receiptRefNum) {
+            this.receiptRefNum = receiptRefNum;
+        }
 
-    /**
-     *
-     * @return
-     * The status
-     */
-    public Integer getStatus() {
-        return status;
-    }
+        /**
+         *
+         * @return
+         * The doctorAdvice
+         */
+        public String getDoctorAdvice() {
+            return doctorAdvice;
+        }
 
-    /**
-     *
-     * @param status
-     * The status
-     */
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
+        /**
+         *
+         * @param doctorAdvice
+         * The doctorAdvice
+         */
+        public void setDoctorAdvice(String doctorAdvice) {
+            this.doctorAdvice = doctorAdvice;
+        }
 
-    /**
-     *
-     * @return
-     * The patientUserId
-     */
-    public String getPatientUserId() {
-        return patientUserId;
-    }
+        /**
+         *
+         * @return
+         * The status
+         */
+        public Integer getStatus() {
+            return status;
+        }
 
-    /**
-     *
-     * @param patientUserId
-     * The patientUserId
-     */
-    public void setPatientUserId(String patientUserId) {
-        this.patientUserId = patientUserId;
-    }
+        /**
+         *
+         * @param status
+         * The status
+         */
+        public void setStatus(Integer status) {
+            this.status = status;
+        }
 
-    /**
-     *
-     * @return
-     * The patientRequirement
-     */
-    public String getPatientRequirement() {
-        return patientRequirement;
-    }
+        /**
+         *
+         * @return
+         * The patientUserId
+         */
+        public String getPatientUserId() {
+            return patientUserId;
+        }
 
-    /**
-     *
-     * @param patientRequirement
-     * The patientRequirement
-     */
-    public void setPatientRequirement(String patientRequirement) {
-        this.patientRequirement = patientRequirement;
-    }
+        /**
+         *
+         * @param patientUserId
+         * The patientUserId
+         */
+        public void setPatientUserId(String patientUserId) {
+            this.patientUserId = patientUserId;
+        }
 
-    /**
-     *
-     * @return
-     * The PatientMeetingPostingID
-     */
-    public String getPatientMeetingPostingID() {
-        return PatientMeetingPostingID;
-    }
+        /**
+         *
+         * @return
+         * The PatientMeetingPostingID
+         */
+        public String getPatientMeetingPostingID() {
+            return PatientMeetingPostingID;
+        }
 
-    /**
-     *
-     * @param PatientMeetingPostingID
-     * The PatientMeetingPostingID
-     */
-    public void setPatientMeetingPostingID(String PatientMeetingPostingID) {
-        this.PatientMeetingPostingID = PatientMeetingPostingID;
-    }
+        /**
+         *
+         * @param PatientMeetingPostingID
+         * The PatientMeetingPostingID
+         */
+        public void setPatientMeetingPostingID(String PatientMeetingPostingID) {
+            this.PatientMeetingPostingID = PatientMeetingPostingID;
+        }
 
-    /**
-     *
-     * @return
-     * The id
-     */
-    public String getId() {
-        return id;
-    }
+        /**
+         *
+         * @return
+         * The location
+         */
+        public String getLocation() {
+            return location;
+        }
 
-    /**
-     *
-     * @param id
-     * The id
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
+        /**
+         *
+         * @param location
+         * The location
+         */
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+        /**
+         *
+         * @return
+         * The specialNeeds
+         */
+        public String getSpecialNeeds() {
+            return specialNeeds;
+        }
+
+        /**
+         *
+         * @param specialNeeds
+         * The specialNeeds
+         */
+        public void setSpecialNeeds(String specialNeeds) {
+            this.specialNeeds = specialNeeds;
+        }
+
+        /**
+         *
+         * @return
+         * The wheelchair
+         */
+        public Boolean getWheelchair() {
+            return wheelchair;
+        }
+
+        /**
+         *
+         * @param wheelchair
+         * The wheelchair
+         */
+        public void setWheelchair(Boolean wheelchair) {
+            this.wheelchair = wheelchair;
+        }
+
+        /**
+         *
+         * @return
+         * The meetingDuration
+         */
+        public Integer getMeetingDuration() {
+            return meetingDuration;
+        }
+
+        /**
+         *
+         * @param meetingDuration
+         * The meetingDuration
+         */
+        public void setMeetingDuration(Integer meetingDuration) {
+            this.meetingDuration = meetingDuration;
+        }
+
+        /**
+         *
+         * @return
+         * The meetingTimeStart
+         */
+        public Integer getMeetingTimeStart() {
+            return meetingTimeStart;
+        }
+
+        /**
+         *
+         * @param meetingTimeStart
+         * The meetingTimeStart
+         */
+        public void setMeetingTimeStart(Integer meetingTimeStart) {
+            this.meetingTimeStart = meetingTimeStart;
+        }
+
+        /**
+         *
+         * @return
+         * The meetingDate
+         */
+        public String getMeetingDate() {
+            return meetingDate;
+        }
+
+        /**
+         *
+         * @param meetingDate
+         * The meetingDate
+         */
+        public void setMeetingDate(String meetingDate) {
+            this.meetingDate = meetingDate;
+        }
+
+        /**
+         *
+         * @return
+         * The district
+         */
+        public Integer getDistrict() {
+            return district;
+        }
+
+        /**
+         *
+         * @param district
+         * The district
+         */
+        public void setDistrict(Integer district) {
+            this.district = district;
+        }
+
+        /**
+         *
+         * @return
+         * The dialect
+         */
+        public List<Integer> getDialect() {
+            return dialect;
+        }
+
+        /**
+         *
+         * @param dialect
+         * The dialect
+         */
+        public void setDialect(List<Integer> dialect) {
+            this.dialect = dialect;
+        }
+
+        /**
+         *
+         * @return
+         * The id
+         */
+        public String getId() {
+            return id;
+        }
+
+        /**
+         *
+         * @param id
+         * The id
+         */
+        public void setId(String id) {
+            this.id = id;
+        }
+
+  
 
 }
 
